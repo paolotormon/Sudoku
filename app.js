74ea9974ed7c7f4f2cf0c04f2e387823c27240a5
Paolo Tormon <paolotormon@gmail.com>
2022-05-17 10:50:40 +0800

    Add input to array

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const puzzleBoard = document.querySelector("#puzzle");
 const solveButton = document.querySelector("#solve-button");
 const squares = 81;
+const submission = [];
 
 for (let i = 0; i < squares; i++) {
   const inputElement = document.createElement("input");
@@ -9,3 +10,14 @@ for (let i = 0; i < squares; i++) {
   inputElement.setAttribute("max", "9");
   puzzleBoard.appendChild(inputElement);
 }
+
+const joinValues = () => {
+  submission.splice(0, submission.length); //clear
+  const inputs = document.querySelectorAll("input");
+  for (const input of inputs) {
+    input.value ? submission.push(input.value) : submission.push(".");
+  }
+  console.log(submission);
+};
+
+solveButton.addEventListener("click", joinValues);

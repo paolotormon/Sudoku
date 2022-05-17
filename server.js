b34c0caacccd2e556efe9cb427d2e98a850a0c1b
Paolo Tormon <paolotormon@gmail.com>
2022-05-17 17:43:05 +0800

    build

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static("/")); 
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
